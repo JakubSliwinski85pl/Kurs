4a1a7db4e31635f08007a6ee195443a5b7336698
Jakub Sliwinski <jakub.sliwinski@gmail.com>
2018-05-08 16:13:42 +0200

    dodane webdriver 1

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace HelloWeb
+{    
+    public class HelloWebTests : IDisposable
+    {
+
+        private readonly string BlogUrl = "https://autotestdotnet.wordpress.com/";
+        private IWebDriver browser;
+
+        public HelloWebTests()
+        {
+            browser = new ChromeDriver();
+        }
+                
+        [Fact]
+        public void CanOpenBlogAndHelloNoteExists()
+        {
+            browser.Navigate().GoToUrl(BlogUrl);
+
+            // var element = browser.FindElement(By.LinkText("Witamy na warsztatach automatyzacji testów!"));
+            var article = browser.FindElement(By.Id("post-3096"));
+            var header = article.FindElement(By.TagName("h1"));
+
+            Assert.Equal("Witamy na warsztatach automatyzacji testów!", header.Text);
+        }
+
+        public void Dispose()
+        {
+          try
+                {
+                    browser.Quit();
+                }
+                catch (Exception) { }
+          }
+    }
+}
