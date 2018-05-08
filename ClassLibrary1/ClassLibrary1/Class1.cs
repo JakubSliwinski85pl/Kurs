aaff944586e1a0ec1ffef0ae281ba5c6533b0186
Jakub Sliwinski <jakub.sliwinski@gmail.com>
2018-05-08 17:43:02 +0200

    cwiczenie 1 z dodawaniem komentarza

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -19,17 +19,67 @@ namespace HelloWeb
         {
             browser = new ChromeDriver();
         }
-                
+
+
+        private string GenerateEmail()
+        {
+            var user = Guid.NewGuid().ToString();
+            return $"{user}@nonexistent.test.com";
+        }
+
+        private string GenerateText()
+        {
+            var randomText = Guid.NewGuid().ToString();
+            return $"{randomText}";
+        }
+
+
         [Fact]
         public void CanOpenBlogAndHelloNoteExists()
         {
             browser.Navigate().GoToUrl(BlogUrl);
 
-            // var element = browser.FindElement(By.LinkText("Witamy na warsztatach automatyzacji testów!"));
-            var article = browser.FindElement(By.Id("post-3096"));
-            var header = article.FindElement(By.TagName("h1"));
 
-            Assert.Equal("Witamy na warsztatach automatyzacji testów!", header.Text);
+
+
+            // var element = browser.FindElement(By.LinkText("Witamy na warsztatach automatyzacji testów!"));
+
+            var article = browser.FindElement(By.Id("post-3096"));
+            var footer = article.FindElement(By.TagName("footer"));
+
+            var commentsLink = footer.FindElement(By.ClassName("comments-link"));
+
+
+            commentsLink.Click();
+
+            var textBox1 = browser.FindElement(By.Name("comment"));
+            textBox1.Click();
+            string randomText = GenerateText();
+            textBox1.SendKeys(randomText);
+
+            var email_textBox1 = browser.FindElement(By.Id("email"));
+          //  email_textBox1.Click();
+            email_textBox1.SendKeys(GenerateEmail());
+
+            var user_textBox1 = browser.FindElement(By.Id("author"));
+           // user_textBox1.Click();
+            user_textBox1.SendKeys("JŚ");
+
+
+            var submi_btn1 = browser.FindElement(By.Name("submit"));
+            submi_btn1.Click();
+
+
+
+            var check_comment = browser.FindElement(By.ClassName("comment-content"));
+
+
+            var p_fromComment = check_comment.FindElements(By.TagName("p"));
+          int ilosc_p =  p_fromComment.Count();
+            string x  = p_fromComment[0].Text;
+
+            
+            Assert.Equal(randomText, x);
         }
 
         public void Dispose()
