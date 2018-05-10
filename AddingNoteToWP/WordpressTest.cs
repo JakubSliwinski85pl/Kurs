2e2e043e6fa131b577039857014c32c9eccade29
Jakub Sliwinski <jakub.sliwinski@gmail.com>
2018-05-10 11:44:22 +0200

    Gotowy test WP dodawanie notki

--- AddingNoteToWP/WordpressTest.cs
+++ AddingNoteToWP/WordpressTest.cs
@@ -14,7 +14,7 @@ namespace AddingNoteToWP
     {
         private const string PageUrl = "https://autotestdotnet.wordpress.com/wp-admin";
         private readonly IWebDriver driver;
-        private readonly ExampleComment testComment;
+       // private readonly ExampleComment testComment;
         private readonly ExampleComment testPost;
         private readonly TestCredentials testCredentials;
 
@@ -32,12 +32,15 @@ namespace AddingNoteToWP
             //arange
             var Page = new Page(driver, PageUrl);
 
-
             //act
             Page.Login(testCredentials);
-            Page.InsertPost(testPost);
-            //assert
+            string PageUrl_to_veryfi = Page.InsertPost(testPost);
+            Page.Logout();
 
+            //assert
+            var Page_v = new Page(driver, PageUrl_to_veryfi);
+            bool AssertResult = Page_v.CheckAssert(testPost);
+            Assert.True(AssertResult);
         }
 
         public void Dispose()
@@ -51,10 +54,6 @@ namespace AddingNoteToWP
                 throw;
             }
         }
-
-
-
-
     }
 
     internal class Page
@@ -71,30 +70,59 @@ namespace AddingNoteToWP
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
         }
 
-        internal void InsertPost(ExampleComment testPost)
+        internal bool CheckAssert(ExampleComment testPost)
         {
+            bool AssertResult = false;
 
+            var AssertTitle = _driver.FindElement(By.ClassName("entry-title")).Text;
+
+            var AssertContent = _driver.FindElement(By.ClassName("entry-content"));
+            var AssertText = AssertContent.FindElement(By.TagName("p")).Text;
+
+           if (testPost.Text == AssertText && testPost.Title == AssertTitle)
+            {
+                AssertResult = true;
+            }
+
+            return AssertResult;
+        }
+
+        internal string InsertPost(ExampleComment testPost)
+        {
             var LeftMenu = _driver.FindElements(By.ClassName("wp-menu-name"));
             var PostBtnFromLeftMenu = LeftMenu.SingleOrDefault(c => c.Text.Contains("Posts"));
             PostBtnFromLeftMenu.Click();
 
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
-
-
-
-
+            
             var SearchingBTN = _driver.FindElement(By.ClassName("page-title-action"));
             SearchingBTN.Click();
 
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+   
+            var TitleField = _driver.FindElement(By.Id("title"));
+            TitleField.Click();
+            TitleField.SendKeys(testPost.Title);
 
+            var TextField = _driver.FindElement(By.Id("content"));
+            TextField.Click();
+            TextField.SendKeys(testPost.Text);
 
+            WaitForClickable(_driver.FindElement(By.Id("edit-slug-buttons")), 100000);
+            var EditBtnWaitFor = _driver.FindElement(By.Id("edit-slug-buttons"));
+            var PublishBtnWaitFor = _driver.FindElement(By.Id("publish"));
 
+            var permamentLink = _driver.FindElement(By.Id("edit-slug-box"));
+            var permamentLink_s = permamentLink.FindElement(By.TagName("a"));
+             string CheckUrl = permamentLink_s.GetAttribute("href");
+
+            PublishBtnWaitFor.Click();
+
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
+                        
+            return CheckUrl;            
         }
-
-
-
-
+        
         internal void Login(TestCredentials TestCredentials)
         {
             WaitForClickable(_driver.FindElement(By.Id("usernameOrEmail")),100000);
@@ -115,20 +143,27 @@ namespace AddingNoteToWP
             var LogInButton = _driver.FindElement(By.ClassName("login__form-action"));
             ContinueButton.Click();
 
-            //wait
             _driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
-
         }
 
+        internal void Logout()
+        {
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
 
+            var Avatar = _driver.FindElement(By.Id("wp-admin-bar-my-account"));
+            Avatar.Click();
+
+            var SingOut_find = _driver.FindElement(By.TagName("form"));
+            SingOut_find.Click();
+
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+        }
 
         private void WaitForClickable(IWebElement by, int seconds)
         {
             var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(seconds));
             wait.Until(ExpectedConditions.ElementToBeClickable(by));
         }
-
-
     }
 
     internal class TestCredentials
@@ -142,20 +177,17 @@ namespace AddingNoteToWP
             Password = "codesprinters2018";
         }
     }
-
-
-
-
+        
     internal class ExampleComment
     {
         public string Name { get; }
-        public string Email { get; }
+        public string Title { get; }
         public string Text { get; }
 
         public ExampleComment()
         {
             Name = GenerateUserName();
-            Email = GenerateEmail();
+            Title = GenerateTitle();
             Text = GenerateComment();
         }
 
@@ -164,15 +196,14 @@ namespace AddingNoteToWP
             return Guid.NewGuid().ToString();
         }
 
-        private string GenerateEmail()
+        private string GenerateTitle()
         {
-            var user = Guid.NewGuid().ToString();
-            return $"{user}@nonexistent.test.com";
+            return "JŚ" + " " + Guid.NewGuid().ToString();
         }
 
         private string GenerateUserName()
         {
-            return "JŚ" + "  " + Guid.NewGuid().ToString();
+            return "JŚ" + " " + Guid.NewGuid().ToString();
         }
     }
 }
